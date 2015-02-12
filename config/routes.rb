fb852176d79d47610b0c25576397802e3c02fa74
Melissa Patterson <melissa.patterson.va@gmail.com>
2015-02-12 16:55:52 -0600

    root to homepage
    Connected the root to a launch page of sorts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  root 'welcome#index'
+  
   resources :tales, except: [:destroy, :edit]
 
   # The priority is based upon order of creation: first created -> highest priority.
