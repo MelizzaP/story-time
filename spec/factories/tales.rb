f9bdf742821bead2d49e541515632a4bd113464e
Melissa Patterson <melissa.patterson.va@gmail.com>
2015-02-13 14:51:35 -0600

    add tales to factory
    Added more tales to the tales factory for testing with sentence
    structure and public access

--- spec/factories/tales.rb
+++ spec/factories/tales.rb
@@ -2,5 +2,20 @@ FactoryGirl.define do
   factory :tale do
     story_type 'word'
     public_access true
+    
+    factory :sentence_tale do 
+      story_type 'sentence'
+    end
+    
+    factory :private_tale do
+      public_access false
+    end
+    
+    factory :full_tale do
+      title 'The foot'
+      content 'My foot is a'
+      inspiration 'A foot'
+    end
+  
   end
 end
